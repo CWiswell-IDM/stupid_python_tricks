57d22e05cebdb7a81888e7aa15a45cd2f92b1960
cwiswell <cwiswell@idmod.org>
2021-02-17 17:22:54 -0800

    Updating to show better usage in example, and a simpler print only example.

--- test_kwargs.py
+++ test_kwargs.py
@@ -1,6 +1,6 @@
 import json
 import unittest
-import kwargs_example
+import kwargs_example_bad
 
 
 class IvKeys:
@@ -32,17 +32,17 @@ class KwargsTest(unittest.TestCase):
     # region helper methods
     @classmethod
     def setUpClass(cls) -> None:
-        cls.default_blocking_iv = kwargs_example.blocking_killing_iv()
+        cls.default_blocking_iv = kwargs_example_bad.blocking_killing_iv()
         cls.default_blocker_killing_effectiveness = \
             cls.default_blocking_iv[IvKeys.Killing_Effectiveness]
         cls.default_blocker_blocking_effectiveness = \
             cls.default_blocking_iv[IvKeys.Blocking_Effectiveness]
 
-        cls.default_killer_iv = kwargs_example.killing_intervention()
+        cls.default_killer_iv = kwargs_example_bad.killing_intervention()
         cls.default_killer_killing_effectiveness = \
             cls.default_killer_iv[IvKeys.Killing_Effectiveness]
 
-        cls.default_iv = kwargs_example.base_intervention()
+        cls.default_iv = kwargs_example_bad.base_intervention()
         cls.default_start_day = cls.default_iv[IvKeys.Start_Day]
         cls.default_coverage = cls.default_iv[IvKeys.Coverage]
 
@@ -89,7 +89,7 @@ class KwargsTest(unittest.TestCase):
 
     # region tests
     def test_default_blocker(self):
-        self.iv_under_test = kwargs_example.blocking_killing_iv()
+        self.iv_under_test = kwargs_example_bad.blocking_killing_iv()
         self.verify_default_parent_params()
         self.verify_default_blocker_params()
         self.assertNotEqual(self.default_killer_killing_effectiveness,
@@ -103,7 +103,7 @@ class KwargsTest(unittest.TestCase):
         return
 
     def test_default_killer(self):
-        self.iv_under_test = kwargs_example.killing_intervention()
+        self.iv_under_test = kwargs_example_bad.killing_intervention()
         self.assertEqual(self.iv_under_test[IvKeys.Killing_Effectiveness],
                          self.default_killer_killing_effectiveness)
         self.verify_default_parent_params()
@@ -118,7 +118,7 @@ class KwargsTest(unittest.TestCase):
     def test_blocker_sets_local(self):
         self.is_debugging = True
         specific_blocking_eff = 0.131
-        self.iv_under_test = kwargs_example.blocking_killing_iv(
+        self.iv_under_test = kwargs_example_bad.blocking_killing_iv(
             blocking_eff=specific_blocking_eff
         )
         self.assertEqual(self.iv_under_test[IvKeys.Blocking_Effectiveness],
@@ -129,7 +129,7 @@ class KwargsTest(unittest.TestCase):
 
     def test_blocker_sets_parent(self):
         specific_start_day = 13
-        self.iv_under_test = kwargs_example.blocking_killing_iv(start_day=specific_start_day)
+        self.iv_under_test = kwargs_example_bad.blocking_killing_iv(start_day=specific_start_day)
         self.assertEqual(self.iv_under_test[IvKeys.Start_Day],
                          specific_start_day)
         self.assertEqual(self.iv_under_test[IvKeys.Coverage],
@@ -139,7 +139,7 @@ class KwargsTest(unittest.TestCase):
 
     def test_blocker_adds_extra_param(self):
         specific_insecticide = "Hairspray"
-        self.iv_under_test = kwargs_example.blocking_killing_iv(
+        self.iv_under_test = kwargs_example_bad.blocking_killing_iv(
             insecticide=specific_insecticide
         )
         self.assertEqual(self.iv_under_test[IvKeys.Insecticide],
